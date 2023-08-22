b04f7c95a2c81f4a5f61c0d573ebc25e5835f639
Luís Arteiro <lucho14garfield@gmail.com>
2023-08-22 13:04:00 +0100

    fix: Fix footer. #1

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -274,13 +274,7 @@ class _HomePageState extends State<HomePage> {
               child: quillEditor,
             ),
           ),
-          kIsWeb
-              ? Expanded(
-                  child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-                  child: toolbar,
-                ))
-              : Container(child: toolbar)
+          Container(child: toolbar)
         ],
       ),
     );
